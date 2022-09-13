c8b3628384e979a0cd7abd307848311b8154eda5
Traci Dukes <tdukes@corporatecostcontrol.com>
2022-09-13 11:33:07 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'esql-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-excel2SQL';
+}
